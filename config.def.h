dd0081ed1a3b813d8af9dab57387ec3015d484b6
pearl <vincent.fortin279@gmail.com>
2024-11-19 22:30:18 -0500

    format, and add BAT0 percent

--- config.def.h
+++ config.def.h
@@ -64,10 +64,10 @@ static const char unknown_str[] = "n/a";
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  */
 static const struct arg args[] = {
-    /* function format          argument */
-    { ipv4, "%s  ", "wlp3s0" },
-    { battery_perc, "%s + ", "BAT0" },
-    { battery_perc, "%s%%  ", "BAT1" },
-    { ram_perc, "RAM %s%%  ", NULL },
+    /* function    format       argument */
+    { ipv4,           "%s  ", "wlp3s0"   },
+    { battery_perc, "%s%% + ", "BAT0"     },
+    { battery_perc, "%s%%  ", "BAT1"     },
+    { ram_perc, "RAM %s%%  ", NULL       },
     { datetime, "%s", "%a, %b %d | %I:%M" }
 };
